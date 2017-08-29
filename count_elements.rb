4833b3e59f08850e1ab114e7be6f22441004f4d7
ChuckBTaylor <WLScripps@gmail.com>
2017-08-29 09:53:59 -0400

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,11 @@
-def count_elements(array)
-  # code goes here
-end
- 
+  def count_elements(array)
+    oohh = {}
+    array.each do |word|
+      if !oohh.has_key?(word)
+        oohh[word] = 1
+      else
+        oohh[word] += 1
+      end
+    end
+    oohh
+  end
